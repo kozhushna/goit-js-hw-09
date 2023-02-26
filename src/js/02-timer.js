c521914ff571d55c4eac63efe9dd47346fbaa717
kozhushna <k.kozhushna@gmail.com>
2023-02-26 12:01:52 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,9 +4,15 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 const refs = {
   startButton: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
 };
 
 refs.startButton.disabled = true;
+
+let choosedDate = null;
 
 const options = {
   enableTime: true,
@@ -20,12 +26,16 @@ const options = {
       alert('Please choose a date in the future');
     } else {
       refs.startButton.disabled = false;
+      choosedDate = selectedDate;
     }
-    // console.log(selectedDates[0]);
   },
 };
 
 flatpickr('#datetime-picker', options);
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -46,6 +56,19 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+refs.startButton.addEventListener('click', e => {
+  refs.startButton.disabled = true;
+  let intervalId = setInterval(() => {
+    const delta = choosedDate - Date.now();
+    if (delta < 0) {
+      clearInterval(intervalId);
+      return;
+    }
+    const { days, hours, minutes, seconds } = convertMs(delta);
+
+    refs.days.textContent = addLeadingZero(days);
+    refs.hours.textContent = addLeadingZero(hours);
+    refs.minutes.textContent = addLeadingZero(minutes);
+    refs.seconds.textContent = addLeadingZero(seconds);
+  }, 1000);
+});
